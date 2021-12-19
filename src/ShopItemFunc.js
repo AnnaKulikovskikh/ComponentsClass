e8261242d95f2e56cae8b2c2bf61a6d2c177e3dc
Anna <k_anna_i@mail.ru>
2021-12-19 22:49:09 +0300

    creation

---
+++ src/ShopItemFunc.js
@@ -0,0 +1,23 @@
+class UnitProfile extends React.Component {
+  constructior (props) {
+    super(props);
+  }
+  render () {
+    const {data} = this.props;
+    <div className={main-content}>
+      <h2>{data.brand}</h2>
+      <h1>{data.title}</h1>
+      <h3>{data.description}</h3>
+      <div className={description}>{data.descriptionFull}</div>
+      <div className={`${highlight-window} ${mobile}`}>
+        <div className={highlight-overlay}></div>
+      </div>
+      <div className={divider}></div>
+      <div className={purchase-info}>
+        <div className={price}>{data.currency}{data.price}</div>
+        <button>Добавить в корзину</button>
+      </div>
+    </div>
+  }
+}
+export default UnitProfile;
